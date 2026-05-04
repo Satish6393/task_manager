d5a047e57c1d72ae54e4c411dd2f02e3231308f5
SATISH SINGH <kingsatishsingh0000@gmail.com>
2026-05-04 17:31:16 +0530

    Fixed Railway port binding issue

--- backend/app.py
+++ backend/app.py
@@ -28,4 +28,5 @@ def home():
     return {"message": "Team Task Manager API Running 🚀"}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
