307b59162190d3016547a9a4b98a59a63e573522
SATISH SINGH <kingsatishsingh0000@gmail.com>
2026-05-05 19:12:56 +0530

    DB config fixed

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,20 @@
+# import os
+
+# class Config:
+#     SECRET_KEY = os.getenv("SECRET_KEY")
+
+#     MYSQL_HOST = os.getenv("MYSQL_HOST")
+#     MYSQL_USER = os.getenv("MYSQL_USER")
+#     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+#     MYSQL_DB = os.getenv("MYSQL_DB")
+
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    MYSQL_HOST = os.environ.get("MYSQL_HOST")
+    MYSQL_USER = os.environ.get("MYSQL_USER")
+    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+    MYSQL_DB = os.environ.get("MYSQL_DB")
+    MYSQL_PORT = int(os.environ.get("MYSQL_PORT", 3306))
 
-    MYSQL_HOST = os.getenv("MYSQL_HOST")
-    MYSQL_USER = os.getenv("MYSQL_USER")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-    MYSQL_DB = os.getenv("MYSQL_DB")
+   
